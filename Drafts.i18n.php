670e89b8f9bde7a8a061e67e154171789661d6d2
Trevor Parscal <tparscal@users.mediawiki.org>
2008-10-24 16:26:44 +0000

    Removed the mustlogin message since it isn't being used.

--- Drafts.i18n.php
+++ Drafts.i18n.php
@@ -19,7 +19,6 @@ $messages['en'] = array(
 	'drafts-view-saved' => 'Saved',
 	'drafts-view-discard' => 'Discard',
 	'drafts-view-login' => 'login',
-	'drafts-view-mustlogin' => 'You must $1 to see your drafts.',
 	'drafts-view-nonesaved' => 'You do not have any drafts saved at this time.',
 	'drafts-view-notice' => 'You have $1 for this page.',
 	'drafts-view-notice-link' => '$1 {{PLURAL:$1|draft|drafts}}',
@@ -30,13 +29,6 @@ Do you want to continue?',
 	'drafts-save-saved' => 'Saved',
 	'drafts-save-error' => 'Error saving draft',
 	'drafts-save-title' => 'Save as a draft [d]',
-);
-
-/** Message documentation (Message documentation)
- * @author Siebrand
- */
-$messages['qqq'] = array(
-	'drafts-view-mustlogin' => '* $1 is a link to [[Special:UserLogin]] with {{msg|drafts-view-login}} as description.',
 );
 
 /** Dutch (Nederlands)
@@ -51,7 +43,6 @@ $messages['nl'] = array(
 	'drafts-view-saved' => 'Opgeslagen',
 	'drafts-view-discard' => 'Verwijderen',
 	'drafts-view-login' => 'Link description for a link to [[Special:UserLogin]] in {{msg|drafts-view-mustlogin}}.',
-	'drafts-view-mustlogin' => 'U moet $1 om uw werkversies te kunnen bekijken.',
 	'drafts-view-nonesaved' => 'U hebt geen opgeslagen werkversies.',
 	'drafts-view-notice' => 'U moet $1 voor deze pagina.',
 	'drafts-view-notice-link' => '$1 {{PLURAL:$1|werkversie|werkversies}}',
