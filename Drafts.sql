bafa22c621c5e8e3ab4760b939589c961f02aaf5
Trevor Parscal <tparscal@users.mediawiki.org>
2008-10-24 00:13:08 +0000

    Added some indexes on commonly sorted columns.

--- Drafts.sql
+++ Drafts.sql
@@ -1,5 +1,5 @@
 create table /*$wgDBPrefix*/drafts (
-    draft_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    draft_id INTEGER AUTO_INCREMENT,
     draft_token INTEGER,
     draft_user INTEGER,
     draft_namespace INTEGER,
@@ -11,5 +11,10 @@ create table /*$wgDBPrefix*/drafts (
     draft_scrolltop INTEGER,
     draft_text BLOB,
     draft_summary TINYBLOB,
-    draft_minoredit BOOL
+    draft_minoredit BOOL,
+    PRIMARY KEY draft_id (draft_id),
+    INDEX draft_user (draft_user),
+    INDEX draft_namespace (draft_namespace),
+    INDEX draft_title (draft_title),
+    INDEX draft_savetime (draft_savetime)
 ) /*$wgDBTableOptions*/;
