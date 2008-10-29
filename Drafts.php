7da8930db1bea069efe1a4fef36f28446ef65c06
Trevor Parscal <tparscal@users.mediawiki.org>
2008-10-29 18:24:00 +0000

    Added license info.

--- Drafts.php
+++ Drafts.php
@@ -4,6 +4,16 @@
  *
  * @file
  * @ingroup Extensions
+ * 
+ * This file contains the main include file for the Drafts extension of
+ * MediaWiki.
+ *
+ * Usage: Add the following line in LocalSettings.php:
+ * require_once( "$IP/extensions/Drafts/Drafts.php" );
+ *
+ * @author Trevor Parscal <tparscal@wikimedia.org>
+ * @license GPL v2
+ * @version 0.1.0
  */
 
 // Check environment
