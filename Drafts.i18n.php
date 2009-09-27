4d7827954b6fbbdce54196cae227a20685e3b6bb
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-09-27 00:25:23 +0000

    Localisation updates for extension messages from translatewiki.net (2009-09-27 00:00 UTC)

--- Drafts.i18n.php
+++ Drafts.i18n.php
@@ -250,25 +250,25 @@ Voleu continuar?",
  * @author Mormegil
  */
 $messages['cs'] = array(
-	'drafts' => 'Návrhy',
-	'drafts-desc' => 'Přidává možnost uložit na server verzi stránky jako [[Special:Drafts|návrh]]',
-	'drafts-view' => 'Zobrazit návrh',
+	'drafts' => 'Koncepty',
+	'drafts-desc' => 'Přidává možnost uložit na server verzi stránky jako [[Special:Drafts|koncept]]',
+	'drafts-view' => 'Zobrazit koncept',
 	'drafts-view-summary' => 'Tato speciální stránka zobrazuje seznam všech existujících konceptů.
 Nepoužité koncepty budou po {{plural:$1|$1 dni|$1 dnech}} automaticky smazány.',
 	'drafts-view-article' => 'Stránka',
-	'drafts-view-existing' => 'Existující návrhy',
+	'drafts-view-existing' => 'Existující koncepty',
 	'drafts-view-saved' => 'Uložené',
 	'drafts-view-discard' => 'Smazat',
-	'drafts-view-nonesaved' => 'Momentálně nemáte uloženy žádné návrhy.',
+	'drafts-view-nonesaved' => 'Momentálně nemáte uloženy žádné koncepty.',
 	'drafts-view-notice' => 'Máte $1 této stránky',
-	'drafts-view-notice-link' => '$1 {{PLURAL:$1|návrh|návrhy|návrhů}}',
+	'drafts-view-notice-link' => '$1 {{plural:$1|koncept|koncepty|konceptů}}',
 	'drafts-view-warn' => 'Pokud odejdete z této stránky, ztratíte všechny neuložené změny této stránky. Chcete pokračovat?',
-	'drafts-save' => 'Uložit tuto verzi jako návrh',
-	'drafts-save-save' => 'Uložit návrh',
+	'drafts-save' => 'Uložit tuto verzi jako koncept',
+	'drafts-save-save' => 'Uložit koncept',
 	'drafts-save-saved' => 'Uložené',
 	'drafts-save-saving' => 'Ukládá se',
-	'drafts-save-error' => 'Chyba při ukládání návrhu',
-	'tooltip-drafts-save' => 'Uložit jako návrh',
+	'drafts-save-error' => 'Chyba při ukládání konceptu',
+	'tooltip-drafts-save' => 'Uložit jako koncept',
 );
 
 /** German (Deutsch)
@@ -1214,6 +1214,9 @@ Czy chcesz kontynuować?',
  */
 $messages['pms'] = array(
 	'drafts' => 'Sbòss',
+	'drafts-desc' => 'A gionta la possibilità ëd salvé vërsion [[Special:Drafts|sbòss]] ëd na pàgina an sël server',
+	'drafts-view' => 'VardaSbòss',
+	'drafts-view-article' => 'Pàgina',
 );
 
 /** Pashto (پښتو)
